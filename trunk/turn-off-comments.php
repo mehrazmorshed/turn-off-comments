f4ec05141b431740c2e6cb6286acac477bef0a1a
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-02-19 14:26:50 +0600

    Update turn-off-comments.php

--- trunk/turn-off-comments.php
+++ trunk/turn-off-comments.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * Plugin Name:       Turn Off Comments - Remove Commenting System, Disable Comment Box and Hide Comments
+ * Plugin Name:       Turn Off Comments - Remove Comment System, Hide Comment Box and Disable Comments
  * Plugin URI:        https://wordpress.org/plugins/turn-off-comments/
  * Description:       Turn Off Comments from your WordPress Website.
- * Version:           1.4.2
- * Tested Up to:      6.4.1
+ * Version:           1.5.3
+ * Tested Up to:      6.4.3
  * Requires at least: 4.4
  * Requires PHP:      7.0
  * Author:            Mehraz Morshed
@@ -31,139 +31,36 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-if( ! defined( 'ABSPATH' ) ) {
-    
-    exit;
-}
+defined( 'ABSPATH' ) or die( 'Access denied.' );
 
-function turn_off_comments_load_textdomain() {
+require_once plugin_dir_path( __FILE__ ) . 'functions.php';
 
-    load_plugin_textdomain( 'turn-off-comments', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
-}
 add_action( 'init', 'turn_off_comments_load_textdomain' );
 
-
-function turn_off_comments_no_wp_comments() {
-
-    wp_die( 'No comments' );
-}
 add_action( 'pre_comment_on_post', 'turn_off_comments_no_wp_comments' ); 
 
-function turn_off_comments_admin_menu() {
-
-    remove_menu_page( 'edit-comments.php' );
-}
 add_action( 'admin_menu', 'turn_off_comments_admin_menu' );
 
-function turn_off_comments_dashboard() {
-
-    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-}
 add_action( 'admin_init', 'turn_off_comments_dashboard' ); 
 
-function turn_off_comments_status() {
-
-    return false;
-}
 add_filter( 'comments_open', 'turn_off_comments_status', 20, 2 ); 
+
 add_filter( 'pings_open', 'turn_off_comments_status', 20, 2 );
 
-function turn_off_comments_hide_existing_comments( $comments ) {
-
-    $comments = array();
-    return $comments;
-}
 add_filter( 'comments_array', 'turn_off_comments_hide_existing_comments', 10, 2 ); 
 
-function turn_off_comments_admin_bar_render() {
-
-    global $wp_admin_bar;
-    $wp_admin_bar->remove_menu( 'comments' );
-}
 add_action('wp_before_admin_bar_render', 'turn_off_comments_admin_bar_render');  
 
-function turn_off_comments_admin_menu_redirect() {
-
-    global $pagenow;
-
-    if ($pagenow === 'edit-comments.php') {
-
-        wp_redirect( admin_url() );
-        exit;
-    }
-}
 add_action('admin_init', 'turn_off_comments_admin_menu_redirect'); 
 
-function turn_off_comments_post_types_support() {
-
-    $post_types = get_post_types();
-
-    foreach ( $post_types as $post_type ) {
-
-        if ( post_type_supports( $post_type, 'comments' ) ) {
-
-            remove_post_type_support( $post_type, 'comments' );
-            remove_post_type_support( $post_type, 'trackbacks' );
-        }
-    }
-}
 add_action( 'admin_init', 'turn_off_comments_post_types_support' ); 
 
-/****/
+add_action( 'wp_head', 'disable_comment_theme_support' );
 
-if( !function_exists( 'disable_comment_theme_support' )) {
-
-    function disable_comment_theme_support() {
-        ?>
-        <style>
-            #comments {
-                display: none;
-            }
-            .nocomments,
-            .no-comments,
-            .has-comments,
-            .post-comments,
-            .comments-link,
-            .comments-area,
-            .comment-respond,
-            .comments-closed,
-            .comments-wrapper,
-            .wp-block-comments,
-            .comments-area__wrapper,
-            .wp-block-post-comments,
-            .wp-block-comments-title,
-            .wp-block-comment-template,
-            .wp-block-comments-query-loop {
-                display: none;
-            }
-            /** Blocksy **/
-            li.meta-comments {
-                display: none;
-            }
-        </style>
-        <?php
-    }
-    add_action( 'wp_head', 'disable_comment_theme_support' );
-}
-
-/****/
-
-function turn_off_comments_activation_hook() {
-
-    set_transient( 'turn-off-comments-notification', true, 5 );
-}
 register_activation_hook( __FILE__, 'turn_off_comments_activation_hook' );
 
-function turn_off_comments_activation_notification() {
-
-    if( get_transient( 'turn-off-comments-notification' ) ) {
-
-        ?>
-        <div class="updated notice is-dismissible">
-            <p><?php esc_attr_e( 'Thank you for installing Turn Off Comments!', 'turn-off-comments' ); ?></p>
-        </div>
-        <?php
-        delete_transient( 'turn-off-comments-notification' );
-    }
-}
 add_action( 'admin_notices', 'turn_off_comments_activation_notification' );
+
+add_action( 'admin_notices', 'new_year_admin_notice' );
+
+add_action( 'wp_ajax_dismiss_custom_plugin_notice', 'dismiss_new_year_notice' );
