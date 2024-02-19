405ecadd01d2b7f8bb7b1c29f2d1591cda690263
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-02-19 14:28:05 +0600

    Add files via upload

---
+++ trunk/uninstall.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * Turn Off Comments Uninstall Script
+ * @package TurnOffComments
+ * Author: Mehraz Morshed
+ * Copyright 2024 Mehraz Morshed
+ *
+ */
+
+// do not call this file directly
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+  exit;
+}
+
+// uninstallation script
+
+/**
+ *
+ * uninstallation functions goes here
+ *
+ */
+
+// admin feedback script
+
+/**
+ *
+ * third party APIs (such as: Freemium)
+ * can be used for receiving feedbacks
+ *
+ */
